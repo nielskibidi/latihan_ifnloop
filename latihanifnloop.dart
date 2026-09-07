ef40094ce8cf2b42eec37dd38f343d87310cd8fd
Nielskibidi <ngnielniel@gmail.com>
2026-09-07 20:25:26 +0700

    update v3

--- latihanifnloop.dart
+++ latihanifnloop.dart
@@ -1,10 +1,18 @@
 void main() {
     int kehadiran = 75;
 
-    if (kehadiran < 75) {
-        print('Tidak bisa ikut ujian');
-        else {
-            print('Bisa ikut ujian')
-        }
-    }
+    // if (kehadiran < 75) {
+    //     print('Tidak bisa ikut ujian');
+    //     else {
+    //         print('Bisa ikut ujian')
+    //     }
+    // }
+
+    // if (kehadiran < 75) {
+    //     print('Tidak bisa ikut ujian')
+    // }
+
+    // if (kehadiran => 75) {
+    //     print('Bisa ikut ujian')
+    // }
 }
